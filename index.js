63f06240afcf7fbb6eaf441b3671646a3b701100
Richard <richard.brown0807@gmail.com>
2022-11-19 11:54:45 -0600

    commit tree 63fe2b396beca21ba375462876ed079a0f23118a

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-function saturdayFun(activity = "roller-skate") {
-    return `This Saturday, I want to ${activity}!`;
+function saturdayFun(activity = 'roller-skate' ) {
+    return activity
 }
 
-function mondayWork(activity = "go to the office") {
-    return `This Monday, I will ${activity}.`;
+console.log(saturdayFun())
+
+function mondayWork(activity = 'go to the office') {
+    return activity
 }
 
-function wrapAdjective(yup = "*") {
-        return function(hello = "special") {
-            return `You are ${yup}${hello}${yup}!` 
-    }
+function wrapAdjective() {
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun(activity = "roller-skate") {
+//     return `This Saturday, I want to ${activity}!`;
+// }
+
+// function mondayWork(activity = "go to the office") {
+//     return `This Monday, I will ${activity}.`;
+// }
+
+// function wrapAdjective(yup = "*") {
+//         return function(hello = "special") {
+//             return `You are ${yup}${hello}${yup}!`;
+//     }
+// }
 
